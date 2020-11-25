4f377ea2faa97cc4878cce7ec1abd47f43142341
lunagomez26 <luvagoba@gmail.com>
2020-11-25 16:49:04 -0500

    Se modificaron estilos y se agrego appoinment

--- src/app/Components/therapist/therapist.component.ts
+++ src/app/Components/therapist/therapist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';  
+import { UserService } from '../../Services/user.service';
 @Component({
   selector: 'app-therapist',
   templateUrl: './therapist.component.html',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TherapistComponent implements OnInit {
 
-  constructor() { }
+  signFormProfessional: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService:UserService
+  ) { }
 
   ngOnInit(): void {
+    this.validator()
+  }
+
+  validator(){
+    this.signFormProfessional = this.formBuilder.group({
+      name: [ '', Validators.required ],
+      lastName: [ '', Validators.required ],
+      email: [ '', [Validators.required, Validators.email] ],
+      password: [ '',[Validators.required, Validators.minLength(10)] ],
+      role: [ 'Professional', Validators.required ],
+      identificationNumber: [ '', Validators.required ],
+    })
+  }
+
+  saveTherapist(){
+    if(this.signFormProfessional.valid){
+      console.log('Mensaje ',this.signFormProfessional.value)
+      this.userService.createUser(this.signFormProfessional.value).subscribe(
+      (professionalCreated) => {
+        console.log(professionalCreated)
+        alert('el usuario se creo correctamente')},
+        (error) =>{
+          console.error('tuvimos un error ->', error)
+
+        }
+      )
+    }else{
+      alert('El formulario es incorrecto')
+    }
   }
 
 }
